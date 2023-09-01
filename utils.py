bde7b3f19ecadf31a265b53c975574d8ace41aa5
beat4ocean <beat4ocean@gmail.com>
2023-09-02 00:59:11 +0800

    优化代码2

--- utils.py
+++ utils.py
@@ -13,6 +13,14 @@ UNK, PAD = '<UNK>', '<PAD>'  # 未知字，padding符号
 
 
 def build_vocab(file_path, tokenizer, max_size, min_freq):
+    """
+    构建词表
+    :param file_path: 数据集文件路径
+    :param tokenizer: 分词器
+    :param max_size: 词表长度限制
+    :param min_freq: 最低词频限制
+    :return: 词表字典
+    """
     vocab_dic = {}
     with open(file_path, 'r', encoding='UTF-8') as f:
         for line in tqdm(f):
@@ -22,14 +30,19 @@ def build_vocab(file_path, tokenizer, max_size, min_freq):
             content = lin.split('\t')[0]
             for word in tokenizer(content):
                 vocab_dic[word] = vocab_dic.get(word, 0) + 1
-        vocab_list = sorted([_ for _ in vocab_dic.items() if _[1] >= min_freq], key=lambda x: x[1], reverse=True)[
-                     :max_size]
+        vocab_list = sorted([_ for _ in vocab_dic.items() if _[1] >= min_freq], key=lambda x: x[1], reverse=True)[:max_size]
         vocab_dic = {word_count[0]: idx for idx, word_count in enumerate(vocab_list)}
         vocab_dic.update({UNK: len(vocab_dic), PAD: len(vocab_dic) + 1})
     return vocab_dic
 
 
 def build_dataset(config, ues_word):
+    """
+    构建数据集
+    :param config: 配置参数
+    :param ues_word: 是否使用词级别分词
+    :return: 词表字典，训练集，验证集，测试集
+    """
     if ues_word:
         tokenizer = lambda x: list(jieba.cut(x))
     else:
@@ -42,6 +55,12 @@ def build_dataset(config, ues_word):
     print(f"Vocab size: {len(vocab)}")
 
     def load_dataset(path, pad_size=32):
+        """
+        加载数据集
+        :param path: 数据集文件路径
+        :param pad_size: 每条数据的长度限制
+        :return: 数据集列表 [([...], 0), ([...], 1), ...]
+        """
         contents = []
         with open(path, 'r', encoding='UTF-8') as f:
             for line in tqdm(f):
@@ -62,7 +81,7 @@ def build_dataset(config, ues_word):
                 for word in token:
                     words_line.append(vocab.get(word, vocab.get(UNK)))
                 contents.append((words_line, int(label), seq_len))
-        return contents  # [([...], 0), ([...], 1), ...]
+        return contents
 
     train = load_dataset(config.train_path, config.pad_size)
     dev = load_dataset(config.dev_path, config.pad_size)
@@ -72,6 +91,12 @@ def build_dataset(config, ues_word):
 
 class DatasetIterater(object):
     def __init__(self, batches, batch_size, device):
+        """
+        数据迭代器类
+        :param batches: 数据集
+        :param batch_size: 每个batch的大小
+        :param device: 设备(GPU或CPU)
+        """
         self.batch_size = batch_size
         self.batches = batches
         self.n_batches = len(batches) // batch_size
@@ -82,6 +107,11 @@ class DatasetIterater(object):
         self.device = device
 
     def _to_tensor(self, datas):
+        """
+        将数据转换为Tensor类型并移动到指定设备
+        :param datas: 数据
+        :return: 转换后的数据
+        """
         x = torch.LongTensor([_[0] for _ in datas]).to(self.device)
         y = torch.LongTensor([_[1] for _ in datas]).to(self.device)
 
@@ -90,6 +120,10 @@ class DatasetIterater(object):
         return (x, seq_len), y
 
     def __next__(self):
+        """
+        获取下一个batch的数据
+        :return: 下一个batch的数据
+        """
         if self.residue and self.index == self.n_batches:
             batches = self.batches[self.index * self.batch_size: len(self.batches)]
             self.index += 1
@@ -106,9 +140,17 @@ class DatasetIterater(object):
             return batches
 
     def __iter__(self):
+        """
+        返回迭代器对象
+        :return: 迭代器对象
+        """
         return self
 
     def __len__(self):
+        """
+        返回迭代器的长度
+        :return: 迭代器的长度
+        """
         if self.residue:
             return self.n_batches + 1
         else:
@@ -116,15 +158,22 @@ class DatasetIterater(object):
 
 
 def build_iterator(dataset, config):
+    """
+    创建数据迭代器对象
+    :param dataset: 数据集
+    :param config: 配置文件
+    :return: 数据迭代器对象
+    """
     iter = DatasetIterater(dataset, config.batch_size, config.device)
     return iter
 
 
 def get_time_dif(start_time):
-    """获取已使用时间"""
+    """
+    获取已使用时间
+    :param start_time: 开始时间
+    :return: 已使用的时间间隔
+    """
     end_time = time.time()
     time_dif = end_time - start_time
     return timedelta(seconds=int(round(time_dif)))
-
-
-
--- utils_fasttext.py
+++ utils_fasttext.py
@@ -8,12 +8,13 @@ import time
 from datetime import timedelta
 import jieba
 
-MAX_VOCAB_SIZE = 10000
-UNK, PAD = '<UNK>', '<PAD>'
+MAX_VOCAB_SIZE = 10000  # 词表长度限制
+UNK, PAD = '<UNK>', '<PAD>'  # 未知字，padding符号
 
 
 def build_vocab(file_path, tokenizer, max_size, min_freq):
-    vocab_dic = {}
+    vocab_dic = {}  # 初始化词汇字典
+
     with open(file_path, 'r', encoding='UTF-8') as f:
         for line in tqdm(f):
             lin = line.strip()
@@ -22,31 +23,43 @@ def build_vocab(file_path, tokenizer, max_size, min_freq):
             content = lin.split('\t')[0]
             for word in tokenizer(content):
                 vocab_dic[word] = vocab_dic.get(word, 0) + 1
+
+        # 根据最小频率限制筛选词汇，并按照词频排序
         vocab_list = sorted([_ for _ in vocab_dic.items() if _[1] >= min_freq], key=lambda x: x[1], reverse=True)[:max_size]
+
+        # 将筛选后的词汇列表转化为词汇字典，其中键是词汇，值是对应的索引
         vocab_dic = {word_count[0]: idx for idx, word_count in enumerate(vocab_list)}
+
+        # 添加未知词标识和填充词标识到词汇字典中，其中索引为词汇字典的长度加上相应的偏移量
         vocab_dic.update({UNK: len(vocab_dic), PAD: len(vocab_dic) + 1})
+
     return vocab_dic
 
 
-def build_dataset(config, ues_word):
-    if ues_word:
-        tokenizer = lambda x: list(jieba.cut(x))
+def build_dataset(config, use_word):
+    if use_word:
+        tokenizer = lambda x: list(jieba.cut(x))  # 使用jieba分词
     else:
-        tokenizer = lambda x: [y for y in x]  # char-level
+        tokenizer = lambda x: [y for y in x]  # 字符级别
+
     if os.path.exists(config.vocab_path):
+        # 加载已构建的词汇表
         vocab = pkl.load(open(config.vocab_path, 'rb'))
     else:
+        # 构建词汇表并保存
         vocab = build_vocab(config.train_path, tokenizer=tokenizer, max_size=MAX_VOCAB_SIZE, min_freq=1)
         pkl.dump(vocab, open(config.vocab_path, 'wb'))
     print(f"Vocab size: {len(vocab)}")
 
     def biGramHash(sequence, t, buckets):
         t1 = sequence[t - 1] if t - 1 >= 0 else 0
+        # 计算二元hash值
         return (t1 * 14918087) % buckets
 
     def triGramHash(sequence, t, buckets):
         t1 = sequence[t - 1] if t - 1 >= 0 else 0
         t2 = sequence[t - 2] if t - 2 >= 0 else 0
+        # 计算三元hash值
         return (t2 * 14918087 * 18408749 + t1 * 14918087) % buckets
 
     def load_dataset(path, pad_size=32):
@@ -66,7 +79,7 @@ def build_dataset(config, ues_word):
                     else:
                         token = token[:pad_size]
                         seq_len = pad_size
-                # word to id
+                # 将词转换为ID
                 for word in token:
                     words_line.append(vocab.get(word, vocab.get(UNK)))
 
@@ -88,18 +101,19 @@ def build_dataset(config, ues_word):
     return vocab, train, dev, test
 
 
-class DatasetIterater(object):
+class DatasetIterator(object):
     def __init__(self, batches, batch_size, device):
         self.batch_size = batch_size
         self.batches = batches
         self.n_batches = len(batches) // batch_size
-        self.residue = False  # 记录batch数量是否为整数 
+        self.residue = False  # 记录batch数量是否为整数
         if len(batches) % batch_size != 0:
             self.residue = True
         self.index = 0
         self.device = device
 
     def _to_tensor(self, datas):
+        # 将数据转换为Tensor并移动到指定的设备
         x = torch.LongTensor([_[0] for _ in datas]).to(self.device)
         y = torch.LongTensor([_[1] for _ in datas]).to(self.device)
         bigram = torch.LongTensor([_[3] for _ in datas]).to(self.device)
@@ -111,15 +125,18 @@ class DatasetIterater(object):
 
     def __next__(self):
         if self.residue and self.index == self.n_batches:
+            # 处理最后一个不完整的batch
             batches = self.batches[self.index * self.batch_size: len(self.batches)]
             self.index += 1
             batches = self._to_tensor(batches)
             return batches
 
         elif self.index >= self.n_batches:
+            # 已遍历完所有完整的batch，抛出StopIteration
             self.index = 0
             raise StopIteration
         else:
+            # 返回下一个完整的batch
             batches = self.batches[self.index * self.batch_size: (self.index + 1) * self.batch_size]
             self.index += 1
             batches = self._to_tensor(batches)
@@ -136,12 +153,13 @@ class DatasetIterater(object):
 
 
 def build_iterator(dataset, config):
-    iter = DatasetIterater(dataset, config.batch_size, config.device)
-    return iter
+    # 构建数据迭代器
+    iterator = DatasetIterator(dataset, config.batch_size, config.device)
+    return iterator
 
 
-def get_time_dif(start_time):
+def get_time_diff(start_time):
     """获取已使用时间"""
     end_time = time.time()
-    time_dif = end_time - start_time
-    return timedelta(seconds=int(round(time_dif)))
+    time_diff = end_time - start_time
+    return timedelta(seconds=int(round(time_diff)))
